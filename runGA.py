001cb26b840d2317fa02036b65765a8d35a8e08f
lok-i <lokesha1b2c3@gmail.com>
2021-04-28 07:00:54 +0000

    Colab compatible,errors resolved and tested

--- runGA.py
+++ runGA.py
@@ -3,80 +3,68 @@ import numpy
 from src.AntennaArray import PatchAntennaArray
 import time
 
-delta_angle_for_integration = 2 #keep it 1 for a better surface plot
-param_opt_range = {'x':{'greater_than':-0.1,'lesser_than':0.1},
-                   'y':{'greater_than':-0.1,'lesser_than':0.1},
-                   'z':{'equal_to':0.},
-                   'A':{'greater_than':0.,'lesser_than':5.},
-                   'beta':{'equal_to':0.},
-                   'W':{'greater_than':1.0e-3,'lesser_than':10.7e-3},
-                   'L':{'greater_than':1.0e-3,'lesser_than':10.47e-3},
-                   'h':{'greater_than':1.0e-3,'lesser_than':3e-3},}
-
-
-PatchArray = PatchAntennaArray(n_patches=50,
-                               Freq=14e9,
-                               Er=2.5,
-                               param_range=param_opt_range)
-
-
-# print('initial_elements:\n',PatchArray.element_array)
-# update_to = [0.,0.,1.,0.,0.,1.]
-# PatchArray.update_array_params(update_to)
-# print('updates_elements:\n',PatchArray.element_array)
-
-steps_per_gen = 0
-no_of_generations_done = 0
-def fitness_func(solution, solution_idx):
-    global steps_per_gen 
-    global no_of_generations_done
-    steps_per_gen +=1
-    # print(steps_per_gen)
-    if steps_per_gen%sol_per_pop == 0:
-        steps_per_gen = 0
-        no_of_generations_done += 1
-        print("Generation:",no_of_generations_done)
-    PatchArray.CalculateFieldSumPatch(dAngleInDeg=delta_angle_for_integration)
-    PatchArray.update_array_params(solution)
-    fitness = PatchArray.get_gain(dAngleInDeg=delta_angle_for_integration)
-    return fitness
-
-fitness_function = fitness_func
-
-num_generations = 20 # Number of generations.
-num_parents_mating = 15 # Number of solutions to be selected as parents in the mating pool.
-
-# To prepare the initial population, there are 2 ways:
-# 1) Prepare it yourself and pass it to the initial_population parameter. This way is useful when the user wants to start the genetic algorithm with a custom initial population.
-# 2) Assign valid integer values to the sol_per_pop and num_genes parameters. If the initial_population parameter exists, then the sol_per_pop and num_genes parameters are useless.
-sol_per_pop = 25 # Number of solutions in the population.
-num_genes = len(PatchArray.params_to_opt_range)
-print("Number of Params to Optimize:",num_genes,'\n')
-gene_ranges = [{'low':p_2_opt_range[0],'high':p_2_opt_range[1]} for p_2_opt_range in PatchArray.params_to_opt_range]
-parent_selection_type = "sss" # Type of parent selection.
-keep_parents = 10 # Number of parents to keep in the next population. -1 means keep all parents and 0 means keep nothing.
-crossover_type = "single_point" # Type of the crossover operator.
-# Parameters of the mutation operation.
-mutation_type = "random" # Type of the mutation operator.
-mutation_percent_genes = 10 # Percentage of genes to mutate. This parameter has no action if the parameter mutation_num_genes exists or when mutation_type is None.
-
-# last_fitness = 0
-# def callback_generation(ga_instance):
-#     global last_fitness
-#     global steps_per_gen 
-#     steps_per_gen = 0
-#     # print("Generation = {generation}".format(generation=ga_instance.generations_completed))
-#     print("Generation",ga_instance.generations_completed)
-#     # print("Best Solution = {bestparams}".format(bestparams=ga_instance.best_solution()[0]))
-#     # print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
-#     # print("Change     = {change}".format(change=ga_instance.best_solution()[1] - last_fitness))
-#     last_fitness = ga_instance.best_solution()[1]
-
-# Creating an instance of the GA class inside the ga module. Some parameters are initialized within the constructor.
-
-
 
 if __name__ == "__main__":
+    delta_angle_for_integration = 2 #keep it 1 for a better surface plot
+    param_opt_range = {'x':{'greater_than':-0.1,'lesser_than':0.1},
+                      'y':{'greater_than':-0.1,'lesser_than':0.1},
+                      'z':{'equal_to':0.},
+                      'A':{'greater_than':0.,'lesser_than':5.},
+                      'beta':{'equal_to':0.},
+                      'W':{'greater_than':1.0e-3,'lesser_than':10.7e-3},
+                      'L':{'greater_than':1.0e-3,'lesser_than':10.47e-3},
+                      'h':{'greater_than':1.0e-3,'lesser_than':3e-3},}
+
+
+    PatchArray = PatchAntennaArray(n_patches=1,
+                                  Freq=14e9,
+                                  Er=2.5,
+                                  param_range=param_opt_range)
+
+
+    # print('initial_elements:\n',PatchArray.element_array)
+    # update_to = [0.,0.,1.,0.,0.,1.]
+    # PatchArray.update_array_params(update_to)
+    # print('updates_elements:\n',PatchArray.element_array)
+
+    steps_per_gen = 0
+    no_of_generations_done = 0
+    def fitness_func(solution, solution_idx):
+        global steps_per_gen 
+        global no_of_generations_done
+        steps_per_gen +=1
+        # print(steps_per_gen)
+        if steps_per_gen%sol_per_pop == 0:
+            steps_per_gen = 0
+            no_of_generations_done += 1
+            print("Generation:",no_of_generations_done)
+        PatchArray.CalculateFieldSumPatch(dAngleInDeg=delta_angle_for_integration)
+        PatchArray.update_array_params(solution)
+        fitness = PatchArray.get_gain(dAngleInDeg=delta_angle_for_integration)
+        return fitness
+
+    fitness_function = fitness_func
+
+    num_generations = 20 # Number of generations.
+    num_parents_mating = 15 # Number of solutions to be selected as parents in the mating pool.
+
+    # To prepare the initial population, there are 2 ways:
+    # 1) Prepare it yourself and pass it to the initial_population parameter. This way is useful when the user wants to start the genetic algorithm with a custom initial population.
+    # 2) Assign valid integer values to the sol_per_pop and num_genes parameters. If the initial_population parameter exists, then the sol_per_pop and num_genes parameters are useless.
+    sol_per_pop = 25 # Number of solutions in the population.
+    num_genes = len(PatchArray.params_to_opt_range)
+    print("Number of Params to Optimize:",num_genes,'\n')
+    gene_ranges = [{'low':p_2_opt_range[0],'high':p_2_opt_range[1]} for p_2_opt_range in PatchArray.params_to_opt_range]
+    parent_selection_type = "sss" # Type of parent selection.
+    keep_parents = 10 # Number of parents to keep in the next population. -1 means keep all parents and 0 means keep nothing.
+    crossover_type = "single_point" # Type of the crossover operator.
+    # Parameters of the mutation operation.
+    mutation_type = "random" # Type of the mutation operator.
+    mutation_percent_genes = 10 # Percentage of genes to mutate. This parameter has no action if the parameter mutation_num_genes exists or when mutation_type is None.
+
+
+
+
     ga_instance = pygad.GA(num_generations=num_generations,
                         num_parents_mating=num_parents_mating, 
                         fitness_func=fitness_function,
@@ -113,7 +101,7 @@ if __name__ == "__main__":
         print("Best fitness value reached after {best_solution_generation} generations.".format(best_solution_generation=ga_instance.best_solution_generation))
 
     # Saving the GA instance.
-    filename = './experiments/50PatchWith_WLH_20Gen' # The filename to which the instance is saved. The name is without extension.
+    filename = './experiments/1PatchWith_WLH_20Gen' # The filename to which the instance is saved. The name is without extension.
     ga_instance.save(filename=filename)
 
     # Loading the saved GA instance.
